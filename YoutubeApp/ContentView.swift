5835110d513d987402f85b76ca22989333eddd5b
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-05-10 02:49:47 -0400

    Initial Commit

---
+++ YoutubeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeApp
+//
+//  Created by Uthman Mohamed on 2021-05-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YoutubeApp/YoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAppApp.swift
+//  YoutubeApp
+//
+//  Created by Uthman Mohamed on 2021-05-10.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
